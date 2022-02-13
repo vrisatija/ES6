82e8cb711de2685332bb01751e72e8cb0f100b17
vri <Vriti_Satija@mckinsey.com>
2022-02-13 22:10:24 +0530

    feat add: Destructuring

---
+++ program5.js
@@ -0,0 +1,5 @@
+let userArray = process.argv.slice(2)
+let [c,a,b] = userArray
+console.log(`{ username: '${a}', email: '${b}' }`)
+//let {width: x, depth} = box;
+// {username: "jdoe", email: "john@doe.com"
